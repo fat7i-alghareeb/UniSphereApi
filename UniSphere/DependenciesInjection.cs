8f75a885a6db03a82aa1bd67858af5c9df596884
fat7i <fat7ighareeb@gmail.com>
2025-06-09 20:20:48 +0300

    ✨ feat: add Docker support to the project
    This commit introduces Docker support to the UniSphere project,
    enabling containerization for easier deployment and scaling.
    
    - Added `.dockerignore` to exclude unnecessary files from the Docker image.
    - Updated `Directory.Packages.props` to include `Microsoft.VisualStudio.Azure.Containers.Tools.Targets`.
    - Added `docker-compose.dcproj`, `docker-compose.yml`, and `docker-compose.override.yml` for Docker Compose orchestration.
    - Added `Dockerfile` to define the container image build process.
    - Updated `UniSphere.Api.csproj` to include Docker-related properties and package references.
    - Updated `DependenciesInjection.cs` to include OpenTelemetry configuration.
    - Added `launchSettings.json` to configure Docker Compose launch settings.
    
    The changes enable the project to be containerized using Docker,
    simplifying deployment and ensuring consistency across different
    environments. The Docker Compose files define the services and
    their configurations, while the Dockerfile specifies the steps
    to build the container image. OpenTelemetry configuration is added
    to support monitoring and tracing in a containerized environment.

--- UniSphere/DependenciesInjection.cs
+++ UniSphere/DependenciesInjection.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using UniSphere.Api.Database;
 
 namespace UniSphere.Api;
@@ -34,8 +38,26 @@ public static class DependenciesInjection
     /// </summary>
     /// <param name="builder"></param>
     /// <returns></returns>
-    public static WebApplicationBuilder AddObservability(this WebApplicationBuilder builder){
+    public static WebApplicationBuilder AddObservability(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddOpenTelemetry()
+            .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+            .WithTracing(tracing => tracing
+                .AddHttpClientInstrumentation()
+                .AddAspNetCoreInstrumentation())
+            .WithMetrics(metrics => metrics
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddRuntimeInstrumentation())
+            .UseOtlpExporter();
 
+        builder.Logging.AddOpenTelemetry(options =>
+            {
+                options.IncludeScopes = true;
+                options.IncludeFormattedMessage = true;
+
+            }
+        );
         return builder;
     }
     /// <summary>
@@ -44,7 +66,8 @@ public static class DependenciesInjection
     /// </summary>
     /// <param name="builder"></param>
     /// <returns></returns>
-    public static WebApplicationBuilder AddApplicationServices(this WebApplicationBuilder builder){
+    public static WebApplicationBuilder AddApplicationServices(this WebApplicationBuilder builder)
+    {
 
         return builder;
     }
