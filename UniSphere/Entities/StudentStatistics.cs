eec7c4ff37bdcf62edc6f7414ea2b44e8247a03a
fat7i <fat7ighareeb@gmail.com>
2025-06-19 08:55:15 +0300

    ✨ (Entities): create StudentStatistics entity
    This commit introduces the StudentStatistics entity.
    The StudentStatistics entity contains information about student's average score, number of attended hours and lectures.

---
+++ UniSphere/Entities/StudentStatistics.cs
@@ -0,0 +1,14 @@
+// using System.ComponentModel.DataAnnotations;
+// using System.ComponentModel.DataAnnotations.Schema;
+
+// namespace UniSphere.Api.Entities; 
+
+// public class StudentStatistics{
+//     public required Guid Id { get; set; }
+//     public required Guid StudentId { get; set; }
+//     public required double Average { get; set; }
+//     public required double NumberOfAttendanceHours { get; set; }
+//     public required int NumberOfAttendanceLectures { get; set; }
+
+//     public StudentCredential Student { get; set; } = null!;
+// }
